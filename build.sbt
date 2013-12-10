204aa881e817d689edd99c386fb370336f34eb14
Francesco Gigli <jaramir@gmail.com>
2013-12-10 01:20:21 +0000

    quit & echo

--- build.sbt
+++ build.sbt
@@ -4,6 +4,9 @@ version := "0.1"
 
 scalaVersion := "2.10.3"
 
-libraryDependencies += "org.pircbotx" % "pircbotx" % "2.0.1"
+libraryDependencies ++= Seq(
+  "org.pircbotx" % "pircbotx" % "2.0.1",
+  "org.slf4j" % "slf4j-simple" % "1.7.5"
+)
 
 mainClass in Compile := Some("it.slack.mario.Mario")
--- src/main/scala/it/slack/mario/Mario.scala
+++ src/main/scala/it/slack/mario/Mario.scala
@@ -1,7 +1,49 @@
 package it.slack.mario
 
+import org.pircbotx.PircBotX
+import org.pircbotx.Configuration
+import org.pircbotx.hooks.Event
+import org.pircbotx.hooks.ListenerAdapter
+import org.pircbotx.hooks.events.MessageEvent
+import org.pircbotx.hooks.events.PrivateMessageEvent
+
+class MainListener extends ListenerAdapter[PircBotX] {
+    val byName = "mario: .*".r
+
+    override def onMessage(event: MessageEvent[PircBotX]) = {
+        val message = event.getMessage
+
+        message match {
+            case byName() =>
+                event.getChannel.send.message(event.getUser, "I'm very busy saving the princess");
+
+            case _ =>
+        }
+    }
+}
+
+class QuitListener extends ListenerAdapter[PircBotX] {
+    override def onPrivateMessage(event: PrivateMessageEvent[PircBotX]) = {
+        if (event.getMessage == "quit") {
+            event.getBot.sendIRC.quitServer
+        }
+    }
+}
+
 object Mario {
     def main(args: Array[String]) = {
-        println("Hello, I'm Mario..")
+        val configuration = new Configuration.Builder()
+            .setName("mario")
+            .setLogin("mario")
+            .setAutoNickChange(true)
+            .setCapEnabled(true)
+            .setServer("localhost", 6697)
+            .addAutoJoinChannel("#testmario")
+            .addListener(new MainListener)
+            .addListener(new QuitListener)
+            .buildConfiguration();
+
+        val bot = new PircBotX(configuration)
+        bot.startBot()
     }
 }
