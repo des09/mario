545cef564367f165b87bb4ecd8db35441f079893
Francesco Gigli <francesco.gigli@timgroup.com>
2013-12-11 14:10:58 +0000

    fgigli: added a configuration file with defaults and local override

--- build.sbt
+++ build.sbt
@@ -6,7 +6,8 @@ scalaVersion := "2.10.3"
 
 libraryDependencies ++= Seq(
   "org.pircbotx" % "pircbotx" % "2.0.1",
-  "org.slf4j" % "slf4j-simple" % "1.7.5"
+  "org.slf4j" % "slf4j-simple" % "1.7.5",
+  "com.typesafe" % "config" % "1.0.2"
 )
 
 mainClass in Compile := Some("it.slack.mario.Mario")
--- src/main/scala/it/slack/mario/Mario.scala
+++ src/main/scala/it/slack/mario/Mario.scala
@@ -6,16 +6,19 @@ import org.pircbotx.hooks.Event
 import org.pircbotx.hooks.ListenerAdapter
 import org.pircbotx.hooks.events.MessageEvent
 import org.pircbotx.hooks.events.PrivateMessageEvent
+import org.pircbotx.UtilSSLSocketFactory
+import com.typesafe.config.ConfigFactory
+import org.slf4j.LoggerFactory
 
-class MainListener extends ListenerAdapter[PircBotX] {
-    val byName = "mario: .*".r
+class MainListener(nickname: String) extends ListenerAdapter[PircBotX] {
+    val byName = (nickname + ": .*") r
 
     override def onMessage(event: MessageEvent[PircBotX]) = {
         val message = event.getMessage
 
         message match {
             case byName() =>
-                event.getChannel.send.message(event.getUser, "I'm very busy saving the princess");
+                event.getChannel.send.message(event.getUser, "I'm very busy saving the princess")
 
             case _ =>
         }
@@ -30,18 +33,49 @@ class QuitListener extends ListenerAdapter[PircBotX] {
     }
 }
 
+class Settings {
+    private val config = ConfigFactory.load()
+
+    // validate vs. reference.conf
+    config.checkValid(ConfigFactory.defaultReference())
+
+    // non-lazy fields, we want all exceptions at construct time
+    val nickname = config.getString("nickname")
+    val username = config.getString("username")
+    val server_hostname = config.getString("server_hostname")
+    val server_port = config.getInt("server_port")
+    val server_uses_ssl = config.getBoolean("server_uses_ssl")
+    val channel = config.getString("channel")
+
+    override def toString: String = {
+        "Settings(nickname: %s, username: %s, server_hostname: %s, server_port: %s, server_uses_ssl: %s, channel: %s)".
+            format(nickname, username, server_hostname, server_port, server_uses_ssl, channel)
+    }
+}
+
 object Mario {
     def main(args: Array[String]) = {
-        val configuration = new Configuration.Builder()
-            .setName("mario")
-            .setLogin("mario")
+        val logger = LoggerFactory.getLogger(getClass)
+
+        val settings = new Settings
+
+        logger.info(settings.toString)
+
+        var configurationBuilder = new Configuration.Builder()
+            .setName(settings.nickname)
+            .setLogin(settings.username)
             .setAutoNickChange(true)
             .setCapEnabled(true)
-            .setServer("localhost", 6697)
-            .addAutoJoinChannel("#testmario")
-            .addListener(new MainListener)
+            .setServer(settings.server_hostname, settings.server_port)
+            .addAutoJoinChannel(settings.channel)
+            .addListener(new MainListener(settings.nickname))
             .addListener(new QuitListener)
-            .buildConfiguration();
+
+        if (settings.server_uses_ssl) {
+            configurationBuilder.setSocketFactory(new UtilSSLSocketFactory().trustAllCertificates())
+        }
+
+        val configuration = configurationBuilder.buildConfiguration()
 
         val bot = new PircBotX(configuration)
         bot.startBot()
